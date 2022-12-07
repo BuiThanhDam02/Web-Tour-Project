5ab5b2614752ee76c878e1922197c9c20553dc61
Nguyễn Hữu Đạt <115572697+hwdat1912@users.noreply.github.com>
2022-12-07 18:45:31 +0700

    update
    update aboutus, aboutus_img
    convert html to jsp

--- webproject/projectWeb/src/main/java/vn/edu/hcmuaf/fit/bean/AboutUsImage.java
+++ webproject/projectWeb/src/main/java/vn/edu/hcmuaf/fit/bean/AboutUsImage.java
@@ -1,4 +1,30 @@
 package vn.edu.hcmuaf.fit.bean;
 
 public class AboutUsImage {
+
+    private String ABOUTUS_ID;
+    private String ImaageURL;
+    //constructor
+    public AboutUsImage(String ABOUTUS_ID, String imaageURL) {
+        this.ABOUTUS_ID = ABOUTUS_ID;
+        ImaageURL = imaageURL;
+    }
+    //setter getter
+    public String getABOUTUS_ID() {
+        return ABOUTUS_ID;
+    }
+
+    public void setABOUTUS_ID(String ABOUTUS_ID) {
+        this.ABOUTUS_ID = ABOUTUS_ID;
+    }
+
+    public String getImaageURL() {
+        return ImaageURL;
+    }
+
+    public void setImaageURL(String imaageURL) {
+        ImaageURL = imaageURL;
+    }
+
+
 }
