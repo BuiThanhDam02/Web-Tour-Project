c5f5ebef15ddc9c0275f633b89240494dcf11f53
hwbi1912 <20130224@st.hcmuaf.edu.vn>
2022-12-18 11:21:44 +0700

    update
    CRUDTourData
    CRUDTourOrderData
    TourOrderTableData
    TourTableData

---
+++ webproject/projectWeb/src/main/java/vn/edu/hcmuaf/fit/controller/admin/CRUDTourData.java
@@ -0,0 +1,33 @@
+package vn.edu.hcmuaf.fit.controller.admin;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import vn.edu.hcmuaf.fit.services.UploadFile;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "CRUDTourData", value = "/admin/CRUDTourData")
+public class CRUDTourData extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    request.getRequestDispatcher("form-add-san-pham.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(fileItemFactory);
+        try {
+            List<FileItem> fileItems = upload.parseRequest(request);
+            new UploadFile().upload(fileItems,request.getServletContext().getRealPath(""));
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        }
+    }
+}
