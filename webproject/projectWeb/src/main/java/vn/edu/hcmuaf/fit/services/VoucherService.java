b48387b53c87fe41e2b195840b63eae2c80f518c
hwbi1912 <20130224@st.hcmuaf.edu.vn>
2022-12-18 10:25:04 +0700

    upload voucherservice

---
+++ webproject/projectWeb/src/main/java/vn/edu/hcmuaf/fit/services/VoucherService.java
@@ -0,0 +1,23 @@
+package vn.edu.hcmuaf.fit.services;
+
+import vn.edu.hcmuaf.fit.DAO.VoucherDAO;
+import vn.edu.hcmuaf.fit.bean.Voucher;
+
+import java.util.List;
+
+public class VoucherService {
+    private static VoucherService instance;
+
+    private VoucherService(){
+
+    }
+
+    public static VoucherService getInstance() {
+        if(instance == null) instance = new VoucherService();
+        return instance;
+    }
+
+    public List<Voucher> getVoucherList(){
+        return VoucherDAO.getInstance().getVoucherList();
+    }
+}
